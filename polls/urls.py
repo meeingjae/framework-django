6a8d0a9dcf53fcb3168a3bf0d29c459c1b5de241
Minjae Kim <minjae.kim@mytrum.com>
2022-09-09 08:59:58 +0900

    feat: Polls url 생성
    * /id
    * /id/custom-results
    * /id/custom-votes

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls
     path('', views.customIndex, name='Custom Index'),
+    # ex: /polls/2
+    path('<int:question_id>', views.detail, name='Details'),
+    # ex: /polls/2/custom-results
+    path('<int:question_id>/custom-results', views.results, name='Custom Results'),
+    # ex: /polls/2/custom-votes
+    path('<int:question_id>/custom-votes', views.vote, name='Custom Voting'),
 ]
