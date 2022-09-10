2c34437d39a6d9f5e9cfcdb188d4a128fe660f72
Minjae Kim <minjae.kim@mytrum.com>
2022-09-10 15:42:45 +0900

    feat: polls index API Version 3 생성

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /polls
-    path('', views.customIndex, name='Custom Index'),
+    path('', views.customIndexV3, name='Custom Index'),
     # ex: /polls/2
     path('<int:question_id>', views.detail, name='Details'),
     # ex: /polls/2/custom-results
